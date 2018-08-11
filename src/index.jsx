995977395c2cf53c65487dbab34b04ef419849af
Vlad Morzhanov <vlad.morzhanov@gmail.com>
2018-08-11 15:59:55 +0300

    Added new webpack configuration with hmr, added env.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
 import { AppContainer } from 'react-hot-loader'
-import './assets/styles/main.scss'
+import './assets/styles/main.styl'
 
 render(
   <AppContainer>
