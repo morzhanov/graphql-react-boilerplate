2e22c84436980d1635987004f38c9e6f3c1f3ba0
Vlad Morzhanov <vlad.morzhanov@gmail.com>
2018-08-19 21:40:50 +0300

    Created NotFound page

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import * as React from 'react'
+import { Paper } from '@material-ui/core'
+import styled from 'styled-components'
 
-const NotFound = () => {
-  return <div>Not found 404</div>
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const mainStyles = {
+  width: '80%',
+  height: '80%',
+  textAlign: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 48
 }
+
+const NotFound = props => (
+  <Wrapper>
+    <Paper style={mainStyles}>404</Paper>
+  </Wrapper>
+)
 
 export default NotFound
